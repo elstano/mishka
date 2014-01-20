609991f912ceff1f454850918d15ca1ad0cc7afe
kidoz <ckidoz@gmail.com>
2014-01-20 15:26:29 +0400

    Added menu item for easy debugging modules

--- source/mishka.web/src/main/java/ru/org/icad/mishka/web/gwt/main/client/menu/MenuDataManager.java
+++ source/mishka.web/src/main/java/ru/org/icad/mishka/web/gwt/main/client/menu/MenuDataManager.java
@@ -1,10 +1,11 @@
 package ru.org.icad.mishka.web.gwt.main.client.menu;
 
+import com.google.common.collect.Lists;
+import ru.org.icad.mishka.web.gwt.main.client.menu.bug.BugPresenter;
 import ru.org.icad.mishka.web.gwt.main.client.menu.theme.MenuRSBundle;
 import ru.org.icad.mishka.web.gwt.main.client.settings.SettingsPresenter;
 import ru.org.icad.mishka.web.gwt.main.client.upload.UploadPresenter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,23 +14,24 @@ import java.util.List;
  * Time: 10:29 PM
  */
 public class MenuDataManager {
-    private List<MenuItem> items = new ArrayList<MenuItem>();
+    private List<MenuItem> items = Lists.newArrayList();
 
     {
-        items.add(new MenuItem(MenuRSBundle.INSTANCE.factories(),"Заводы", new UploadPresenter("о заводах")));
-        items.add(new MenuItem(MenuRSBundle.INSTANCE.products(),"Продукты", new UploadPresenter("о продуктах")));
-        items.add(new MenuItem(MenuRSBundle.INSTANCE.nsi(),"НСИ", new UploadPresenter("об НСИ")));
-        items.add(new MenuItem(MenuRSBundle.INSTANCE.schedule(),"Расписание ЛА", new UploadPresenter("о расписании ЛА")));
-        items.add(new MenuItem(MenuRSBundle.INSTANCE.filter(),"Фильтр", new UploadPresenter("о фильтрах")));
-        items.add(new MenuItem(MenuRSBundle.INSTANCE.sgp(),"СГП/НЗП", new UploadPresenter("об СГП/НЗП")));
-        items.add(new MenuItem(MenuRSBundle.INSTANCE.chemistry(),"Химия Сырца", new UploadPresenter("о химии сырца")));
-        items.add(new MenuItem(MenuRSBundle.INSTANCE.transport(),"Транспорт", new UploadPresenter("о транспорте")));
-        items.add(new MenuItem(MenuRSBundle.INSTANCE.orders(),"Пакет Заказов", new UploadPresenter("о пакете заказов")));
-        items.add(new MenuItem(MenuRSBundle.INSTANCE.settings(),"Расчетный Модуль", new SettingsPresenter()));
-        items.add(new MenuItem(MenuRSBundle.INSTANCE.resultSchedule(),"Расписание", null));
+        items.add(new MenuItem(MenuRSBundle.INSTANCE.factories(), "Заводы", new UploadPresenter("о заводах")));
+        items.add(new MenuItem(MenuRSBundle.INSTANCE.products(), "Продукты", new UploadPresenter("о продуктах")));
+        items.add(new MenuItem(MenuRSBundle.INSTANCE.nsi(), "НСИ", new UploadPresenter("об НСИ")));
+        items.add(new MenuItem(MenuRSBundle.INSTANCE.schedule(), "Расписание ЛА", new UploadPresenter("о расписании ЛА")));
+        items.add(new MenuItem(MenuRSBundle.INSTANCE.filter(), "Фильтр", new UploadPresenter("о фильтрах")));
+        items.add(new MenuItem(MenuRSBundle.INSTANCE.sgp(), "СГП/НЗП", new UploadPresenter("об СГП/НЗП")));
+        items.add(new MenuItem(MenuRSBundle.INSTANCE.chemistry(), "Химия Сырца", new UploadPresenter("о химии сырца")));
+        items.add(new MenuItem(MenuRSBundle.INSTANCE.transport(), "Транспорт", new UploadPresenter("о транспорте")));
+        items.add(new MenuItem(MenuRSBundle.INSTANCE.orders(), "Пакет Заказов", new UploadPresenter("о пакете заказов")));
+        items.add(new MenuItem(MenuRSBundle.INSTANCE.settings(), "Расчетный Модуль", new SettingsPresenter()));
+        items.add(new MenuItem(MenuRSBundle.INSTANCE.resultSchedule(), "Расписание", null));
+        items.add(new MenuItem(MenuRSBundle.INSTANCE.bug(), "Отладка", new BugPresenter()));
     }
 
-    public List<MenuItem> getMenuItems(){
+    public List<MenuItem> getMenuItems() {
         return items;
     }
 }
--- source/mishka.web/src/main/java/ru/org/icad/mishka/web/gwt/main/client/menu/theme/MenuRSBundle.java
+++ source/mishka.web/src/main/java/ru/org/icad/mishka/web/gwt/main/client/menu/theme/MenuRSBundle.java
@@ -40,4 +40,6 @@ public interface MenuRSBundle extends ClientBundle {
     ImageResource transport();
 
     ImageResource resultSchedule();
+
+    ImageResource bug();
 }
