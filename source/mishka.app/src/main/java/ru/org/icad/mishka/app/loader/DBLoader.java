ef1a0a6320ea19af4de04156a869bfd778fe3349
kidoz <ckidoz@gmail.com>
2014-01-24 02:24:37 +0400

    Update db loader

--- source/mishka.app/src/main/java/ru/org/icad/mishka/app/loader/DBLoader.java
+++ source/mishka.app/src/main/java/ru/org/icad/mishka/app/loader/DBLoader.java
@@ -1,8 +1,7 @@
 package ru.org.icad.mishka.app.loader;
 
 import ru.org.icad.mishka.app.TableName;
-import ru.org.icad.mishka.app.loader.excel.CastLoader;
-import ru.org.icad.mishka.app.loader.excel.PlantLoader;
+import ru.org.icad.mishka.app.loader.excel.*;
 import ru.org.icad.mishka.app.util.ExcelUtil;
 
 import javax.naming.InitialContext;
@@ -10,9 +9,11 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.transaction.UserTransaction;
+import java.io.File;
 import java.util.List;
 
 public class DBLoader {
+
     public static void load(String filePath) {
         List<String> sheetNames = ExcelUtil.getSheetNames(filePath);
 
@@ -24,10 +25,44 @@ public class DBLoader {
                 continue;
             }
 
+            if (TableName.MARK.equals(sheetName)) {
+                MarkLoader markLoader = new MarkLoader();
+                persist(markLoader.load(filePath, TableName.MARK));
+
+                continue;
+            }
+
             if (TableName.CAST.equals(sheetName)) {
                 CastLoader castLoader = new CastLoader();
                 persist(castLoader.load(filePath, TableName.CAST));
+
+                continue;
             }
+
+            if (TableName.FORM.equals(sheetName)) {
+                FormLoader formLoader = new FormLoader();
+                persist(formLoader.load(filePath, TableName.FORM));
+
+                continue;
+            }
+
+            if(TableName.CAST_HOUSE.equals(sheetName)) {
+                CastHouseLoader castHouseLoader = new CastHouseLoader();
+                persist(castHouseLoader.load(filePath, TableName.CAST_HOUSE));
+
+                continue;
+            }
+
+            if (TableName.CASTING_UNIT.equals(sheetName)) {
+                CastingUnitLoader castingUnitLoader = new CastingUnitLoader();
+                persist(castingUnitLoader.load(filePath, TableName.CASTING_UNIT));
+
+            }
+        }
+
+        File file = new File(filePath);
+        if (file.exists()) {
+            file.delete();
         }
     }
 
