1bca8a8c6c6b84fd22f82119ae47877338eabc32
kidoz <ckidoz@gmail.com>
2014-02-02 19:38:23 +0400

    Add constant class for state names

---
+++ source/mishka.app/src/main/java/ru/org/icad/mishka/app/StateName.java
@@ -0,0 +1,11 @@
+package ru.org.icad.mishka.app;
+
+public class StateName {
+
+    public static final String READY = "ready";
+    public static final String ENGAGED = "engaged";
+    public static final String PREPARED = "prepared";
+
+    private StateName() {
+    }
+}
