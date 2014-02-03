948a5e93e9fd03c0e86a9e65f04c868086220302
kidoz <ckidoz@gmail.com>
2014-02-03 22:48:04 +0400

    Update casting process

--- source/mishka.app/src/main/java/ru/org/icad/mishka/app/process/casting/CastingProcess.java
+++ source/mishka.app/src/main/java/ru/org/icad/mishka/app/process/casting/CastingProcess.java
@@ -18,14 +18,14 @@ public class CastingProcess {
 
    private static Map<Integer, Queue<? extends Operation>> operationMap = Maps.newConcurrentMap();
 
-    static {
-        operationMap.put(1, Queues.newConcurrentLinkedQueue(Schema1_2.getInitOperations()));
-        operationMap.put(2, Queues.newConcurrentLinkedQueue(Schema1_2.getInitOperations()));
-        operationMap.put(3, Queues.newConcurrentLinkedQueue(Schema3.getInitOperations()));
-        operationMap.put(4, Queues.newConcurrentLinkedQueue(Schema4.getInitOperations()));
-        operationMap.put(5, Queues.newConcurrentLinkedQueue(Schema5_6.getInitOperations()));
-        operationMap.put(6, Queues.newConcurrentLinkedQueue(Schema5_6.getInitOperations()));
-    }
+//    static {
+//        operationMap.put(1, Queues.newConcurrentLinkedQueue(Schema1_2.getInitOperations()));
+//        operationMap.put(2, Queues.newConcurrentLinkedQueue(Schema1_2.getInitOperations()));
+//        operationMap.put(3, Queues.newConcurrentLinkedQueue(Schema3.getInitOperations()));
+//        operationMap.put(4, Queues.newConcurrentLinkedQueue(Schema4.getInitOperations()));
+//        operationMap.put(5, Queues.newConcurrentLinkedQueue(Schema5_6.getInitOperations()));
+//        operationMap.put(6, Queues.newConcurrentLinkedQueue(Schema5_6.getInitOperations()));
+//    }
 
     private CastingUnit castingUnit;
     private List<Cast> casts;
@@ -45,11 +45,11 @@ public class CastingProcess {
         }
 
         Queue<? extends Operation> operations = operationMap.get(castingUnit.getId());
-
-        for(CastWrapper castWrapper: castWrappers) {
-           Operation operation = operations.poll();
-            operation.init(castWrapper);
-            operation.activate();
-        }
+//
+//        for(CastWrapper castWrapper: castWrappers) {
+//           Operation operation = operations.poll();
+//            operation.init(castWrapper);
+//            operation.activate();
+//        }
     }
 }
