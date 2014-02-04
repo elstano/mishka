b1b13be3c97dda40a05d8a1c23eeb58da2675fbb
kidoz <ckidoz@gmail.com>
2014-02-04 22:55:15 +0400

    Add  list with Saz schemes

--- source/mishka.app/src/main/java/ru/org/icad/mishka/app/dev/CastingDev.java
+++ source/mishka.app/src/main/java/ru/org/icad/mishka/app/dev/CastingDev.java
@@ -12,9 +12,10 @@ import ru.org.icad.mishka.app.process.casting.schema3.Schema3;
 import ru.org.icad.mishka.app.process.casting.schema4.Schema4;
 import ru.org.icad.mishka.app.process.casting.schema5_6.Schema5_6;
 
-import javax.naming.InitialContext;
-import javax.persistence.*;
-import javax.transaction.UserTransaction;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TemporalType;
+import javax.persistence.TypedQuery;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -26,6 +27,8 @@ public class CastingDev {
     private static final String START_DATE = "01/05/2013";
     private static final String END_DATE = "01/06/2013";
 
+    private static final List<Integer> SAZ_CAST_UNIT = Lists.newArrayList(22, 24, 26,28, 30, 31, 33);
+
     private static final Map<Integer, Schema> CAST_UNIT_SCHEMA_MAP = ImmutableMap.<Integer, Schema>builder()
             .put(1, new Schema1_2())
             .put(2, new Schema3())
@@ -33,25 +36,17 @@ public class CastingDev {
             .put(4, new Schema5_6())
             .build();
 
+    @PersistenceContext(unitName = "MishkaService")
+    private EntityManager em;
+
     public void castingProcess() {
-        List<Cast> casts = Lists.newArrayList();
         Date startDate = stringToDate(START_DATE);
         Date endDate = stringToDate(END_DATE);
-        try {
-            UserTransaction transaction = (UserTransaction) new InitialContext().lookup("java:comp/UserTransaction");
-            transaction.begin();
-            EntityManagerFactory emf = Persistence.createEntityManagerFactory("MishkaService");
-            EntityManager em = emf.createEntityManager();
-            TypedQuery<Cast> typedQuery = em.createQuery(CAST_QUERY, Cast.class);
-            typedQuery.setParameter("startDate", startDate, TemporalType.DATE);
-            typedQuery.setParameter("endDate", endDate, TemporalType.DATE);
 
-            casts = typedQuery.getResultList();
-            transaction.commit();
-
-        } catch (Exception e) {
-            // empty
-        }
+        TypedQuery<Cast> typedQuery = em.createNamedQuery("Cast.getCastsBetweenDate", Cast.class);
+        typedQuery.setParameter("startDate", startDate, TemporalType.DATE);
+        typedQuery.setParameter("endDate", endDate, TemporalType.DATE);
+        List<Cast> casts = typedQuery.getResultList();
 
         for (Cast cast : casts) {
             Schema schema = CAST_UNIT_SCHEMA_MAP.get(cast.getCastingUnit().getId());
