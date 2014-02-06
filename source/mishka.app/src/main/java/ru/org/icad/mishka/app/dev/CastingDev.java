b9fcae979dab81e36568aa1b66b486ad90697aef
kidoz <ckidoz@gmail.com>
2014-02-06 06:59:42 +0400

    Update casting dev tool

--- source/mishka.app/src/main/java/ru/org/icad/mishka/app/dev/CastingDev.java
+++ source/mishka.app/src/main/java/ru/org/icad/mishka/app/dev/CastingDev.java
@@ -7,10 +7,10 @@ import ru.org.icad.mishka.app.model.Cast;
 import ru.org.icad.mishka.app.process.casting.CastWrapper;
 import ru.org.icad.mishka.app.process.casting.Operation;
 import ru.org.icad.mishka.app.process.casting.Schema;
-import ru.org.icad.mishka.app.process.casting.schema1_2.Schema1_2;
-import ru.org.icad.mishka.app.process.casting.schema3.Schema3;
 import ru.org.icad.mishka.app.process.casting.schema4.Schema4;
-import ru.org.icad.mishka.app.process.casting.schema5_6.Schema5_6;
+import ru.org.icad.mishka.app.process.casting.schema5.Schema5;
+import ru.org.icad.mishka.app.process.casting.schema6.Schema6;
+import ru.org.icad.mishka.app.process.casting.schema9.Schema9;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -23,17 +23,20 @@ import java.util.List;
 import java.util.Map;
 
 public class CastingDev {
-    private static final String CAST_QUERY = "SELECT c FROM Cast c  WHERE c.castDate >= :startDate AND c.castDate < :endDate ORDER BY c.castNumber DESC";
     private static final String START_DATE = "01/05/2013";
     private static final String END_DATE = "01/06/2013";
 
     private static final List<Integer> SAZ_CAST_UNIT = Lists.newArrayList(22, 24, 26,28, 30, 31, 33);
 
     private static final Map<Integer, Schema> CAST_UNIT_SCHEMA_MAP = ImmutableMap.<Integer, Schema>builder()
-            .put(1, new Schema1_2())
-            .put(2, new Schema3())
-            .put(3, new Schema4())
-            .put(4, new Schema5_6())
+            .put(30, new Schema4())
+            .put(33, new Schema4())
+            .put(26, new Schema5())
+            .put(28, new Schema5())
+            .put(22, new Schema6())
+            .put(24, new Schema6())
+            .put(31, new Schema9())
+
             .build();
 
     @PersistenceContext(unitName = "MishkaService")
@@ -55,7 +58,7 @@ public class CastingDev {
 
         for (Schema schema : CAST_UNIT_SCHEMA_MAP.values()) {
             final Operation operation = schema.getInitOperations().iterator().next();
-            operation.setCastWrappers(schema.getQueueCastWrapper());
+            operation.setCastWrappers(schema.getDequeCastWrapper());
             operation.activate();
         }
     }
