bacb629f119be3113d40f25374701a3e650b47ab
kidoz <ckidoz@gmail.com>
2014-02-10 00:56:42 +0400

    Rename restriction by raw to cob

--- source/mishka.app/src/test/java/ru/org/icad/mishka/app/process/cob/RestrictionByCobTest.java
+++ source/mishka.app/src/test/java/ru/org/icad/mishka/app/process/cob/RestrictionByCobTest.java
@@ -1,10 +1,8 @@
-package ru.org.icad.mishka.app.process.raw;
+package ru.org.icad.mishka.app.process.cob;
 
 import org.testng.annotations.Test;
 import ru.org.icad.mishka.app.loader.excel.CastLoader;
-import ru.org.icad.mishka.app.loader.excel.CastLoaderTest;
 import ru.org.icad.mishka.app.loader.excel.ElectrolizerPrognosisLoader;
-import ru.org.icad.mishka.app.loader.excel.ElectrolizerPrognosisLoaderTest;
 import ru.org.icad.mishka.app.model.Cast;
 import ru.org.icad.mishka.app.model.ElectrolizerPrognosis;
 
@@ -17,25 +15,25 @@ import java.util.Map;
 import static org.testng.Assert.assertTrue;
 
 
-public class RestrictionByRawTest {
+public class RestrictionByCobTest {
 
 
     @Test
     public void testCheckRestriction() throws UnsupportedEncodingException {
-        URL urlCast = RestrictionByRawTest.class.getResource("/последовательность ходок.xlsx");
+        URL urlCast = RestrictionByCobTest.class.getResource("/последовательность ходок.xlsx");
         String castFilePath = URLDecoder.decode(urlCast.getPath(), "UTF-8");
 
         CastLoader castLoader = new CastLoader();
         List<Cast> casts = castLoader.load(castFilePath, "CAST");
 
-        URL urlEletrolizer = RestrictionByRawTest.class.getResource("/init/dictionaries/excelTables.xlsx");
+        URL urlEletrolizer = RestrictionByCobTest.class.getResource("/init/dictionaries/excelTables.xlsx");
         String eletrolizerFilePath = URLDecoder.decode(urlEletrolizer.getPath(), "UTF-8");
 
         ElectrolizerPrognosisLoader electrolizerPrognosisLoader = new ElectrolizerPrognosisLoader();
         List<ElectrolizerPrognosis> electrolizerPrognosises
                 = electrolizerPrognosisLoader.load(eletrolizerFilePath, "ELECTROLIZER_PROGNOSIS");
 
-        Map<Cast, List<ElectrolizerPrognosis>> castListMap =  RestrictionByRaw.checkRestriction(casts, electrolizerPrognosises);
+        Map<Cast, List<ElectrolizerPrognosis>> castListMap =  RestrictionByCob.checkRestriction(casts, electrolizerPrognosises);
 
         assertTrue(castListMap.size() == 10);
     }
