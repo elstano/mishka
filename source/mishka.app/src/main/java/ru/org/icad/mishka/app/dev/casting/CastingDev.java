18c1bddba03ad58b4fd4c7d34fb4eaca2c04056c
kidoz <ckidoz@gmail.com>
2014-02-16 21:15:39 +0400

    Update casting dev

--- source/mishka.app/src/main/java/ru/org/icad/mishka/app/dev/casting/CastingDev.java
+++ source/mishka.app/src/main/java/ru/org/icad/mishka/app/dev/casting/CastingDev.java
@@ -1,61 +1,46 @@
 package ru.org.icad.mishka.app.dev.casting;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableMap;
+import ru.org.icad.mishka.app.process.casting.Schema;
 import ru.org.icad.mishka.app.process.casting.SchemaConfiguration;
 import ru.org.icad.mishka.app.process.casting.schema4.Schema4;
 import ru.org.icad.mishka.app.process.casting.schema5_6.Schema5_6;
 import ru.org.icad.mishka.app.process.casting.schema9.Schema9;
 
-import java.util.List;
+import java.util.Map;
 
 public class CastingDev {
 
-    private static final List<Integer> SAZ_CAST_UNIT = Lists.newArrayList(22, 24, 26, 28, 30, 31, 33);
+    public static final Map<Integer, Schema> SCHEMA_MAP = ImmutableMap.<Integer, Schema>builder()
+            .put(30, new Schema4(new SchemaConfiguration(2, 30, new int[]{49}, new int[]{46}, 32)))
+            .put(33, new Schema4(new SchemaConfiguration(2, 33, new int[]{52}, new int[]{51}, 33)))
+            .put(22, new Schema5_6(new SchemaConfiguration(2, 22, new int[]{40, 41}, new int[]{41}, 1)))
+            .put(24, new Schema5_6(new SchemaConfiguration(2, 24, new int[]{42, 43}, new int[]{42}, 1)))
+            .put(26, new Schema5_6(new SchemaConfiguration(2, 26, new int[]{44, 45}, new int[]{43}, 23)))
+            .put(28, new Schema5_6(new SchemaConfiguration(2, 28, new int[]{46, 47}, new int[]{44}, 23)))
+            .put(31, new Schema9(new SchemaConfiguration(2, 31, new int[]{50, 51}, new int[]{48, 50, 49}, 30)))
+            .build();
 
     public void castingProcess() {
-        int castHouseId = 2;
-
-        int[] collectorIds30 = new int[]{49};
-        int[] castingMachineIds30 = new int[]{46};
-        int mouldId30 = 32;
-
-        CastingProcess castingProcess30 = new CastingProcess(new Schema4(new SchemaConfiguration(castHouseId, 30, collectorIds30, castingMachineIds30, mouldId30)));
+        CastingProcess castingProcess30 = new CastingProcess(SCHEMA_MAP.get(30));
         castingProcess30.castingProcess();
 
-        int[] collectorIds33 = new int[]{52};
-        int[] castingMachineIds33 = new int[]{51};
-        int mouldId33 = 34;
-        CastingProcess castingProcess33 = new CastingProcess(new Schema4(new SchemaConfiguration(castHouseId, 33, collectorIds33, castingMachineIds33, mouldId33)));
+        CastingProcess castingProcess33 = new CastingProcess(SCHEMA_MAP.get(33));
         castingProcess33.castingProcess();
 
-        int[] collectorIds22 = new int[]{40, 41};
-        int[] castingMachineIds22 = new int[]{41};
-        int mouldId22 = 1;
-        CastingProcess castingProcess22 = new CastingProcess(new Schema5_6(new SchemaConfiguration(castHouseId, 22, collectorIds22, castingMachineIds22, mouldId22)));
+        CastingProcess castingProcess22 = new CastingProcess(SCHEMA_MAP.get(22));
         castingProcess22.castingProcess();
 
-        int[] collectorIds24 = new int[]{42, 43};
-        int[] castingMachineIds24 = new int[]{42};
-        int mouldId24 = 1;
-        CastingProcess castingProcess24 = new CastingProcess(new Schema5_6(new SchemaConfiguration(castHouseId, 24, collectorIds24, castingMachineIds24, mouldId24)));
+        CastingProcess castingProcess24 = new CastingProcess(SCHEMA_MAP.get(24));
         castingProcess24.castingProcess();
 
-        int[] collectorIds26 = new int[]{44, 45};
-        int[] castingMachineIds26 = new int[]{43};
-        int mouldId26 = 23;
-        CastingProcess castingProcess26 = new CastingProcess(new Schema5_6(new SchemaConfiguration(castHouseId, 26, collectorIds26, castingMachineIds26, mouldId26)));
+        CastingProcess castingProcess26 = new CastingProcess(SCHEMA_MAP.get(26));
         castingProcess26.castingProcess();
 
-        int[] collectorIds28 = new int[]{46, 47};
-        int[] castingMachineIds28 = new int[]{44};
-        int mouldId28 = 23;
-        CastingProcess castingProcess28 = new CastingProcess(new Schema5_6(new SchemaConfiguration(castHouseId, 28, collectorIds28, castingMachineIds28, mouldId28)));
+        CastingProcess castingProcess28 = new CastingProcess(SCHEMA_MAP.get(28));
         castingProcess28.castingProcess();
 
-        int[] collectorIds31 = new int[]{50, 51};
-        int[] castingMachineIds31 = new int[]{48, 50, 49};
-        int mouldId31 = 30;
-        CastingProcess castingProcess31 = new CastingProcess(new Schema9(new SchemaConfiguration(castHouseId, 31, collectorIds31, castingMachineIds31, mouldId31)));
+        CastingProcess castingProcess31 = new CastingProcess(SCHEMA_MAP.get(31));
         castingProcess31.castingProcess();
     }
 }
